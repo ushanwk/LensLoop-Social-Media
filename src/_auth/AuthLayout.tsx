08aca7d0a10e1ef21987e5addda595356cbb08fc
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-28 12:58:20 +0530

    sign in setup

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -9,9 +9,11 @@ const AuthLayout = () => {
         <Navigate to="/" />
       ) : (
         <>
-          <section>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
+
+          <img className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat" src="/assets/images/side-img.svg" alt="logo" />
         </>
       )}
     </>
