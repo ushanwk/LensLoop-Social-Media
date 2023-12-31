d8d6f4ebb3461e0e3fdd3135138cdc185c70a0ce
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-31 15:22:09 +0530

    like posts

--- src/lib/react-query/queryKeys.ts
+++ src/lib/react-query/queryKeys.ts
@@ -1,4 +1,13 @@
 export enum QUERY_KEYS{
-    GET_RECENT_POSTS = 'getRecentPosts',
-    GET_POST_BY_ID = ''
+    // AUTH KEYS
+    CREATE_USER_ACCOUNT = "createUserAccount",
+
+    // USER KEYS
+    GET_CURRENT_USER = "getCurrentUser",
+
+    // POST KEYS
+    GET_POSTS = "getPosts",
+    GET_RECENT_POSTS = "getRecentPosts",
+    GET_POST_BY_ID = "getPostById",
+
 }
