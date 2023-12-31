3846ca557e757c3802e6213242f58acf2460188b
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-31 23:12:39 +0530

    saved posts

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,7 +1,47 @@
+import { Models } from "appwrite";
+import {Loader} from "@/components/shared/Loader.tsx";
+import GridPostList from "@/components/shared/GridPostList.tsx";
+import {useGetCurrentUser} from "@/lib/react-query/queriesandmutations.ts";
+
+
 const Saved = () => {
+    const { data: currentUser } = useGetCurrentUser();
+
+    const savePosts = currentUser?.save
+        .map((savePost: Models.Document) => ({
+            ...savePost.post,
+            creator: {
+                imageUrl: currentUser.imageUrl,
+            },
+        }))
+        .reverse();
+
     return (
-        <>Saved</>
+        <div className="saved-container">
+            <div className="flex gap-2 w-full max-w-5xl">
+                <img
+                    src="/assets/icons/save.svg"
+                    width={36}
+                    height={36}
+                    alt="edit"
+                    className="invert-white"
+                />
+                <h2 className="h3-bold md:h2-bold text-left w-full">Saved Posts</h2>
+            </div>
+
+            {!currentUser ? (
+                <Loader />
+            ) : (
+                <ul className="w-full flex justify-center max-w-5xl gap-9">
+                    {savePosts.length === 0 ? (
+                        <p className="text-light-4">No available posts</p>
+                    ) : (
+                        <GridPostList posts={savePosts} showStats={false} />
+                    )}
+                </ul>
+            )}
+        </div>
     );
 };
 
-export default Saved
+export default Saved;
